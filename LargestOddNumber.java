8f54e080a6b11270698b557772f3bc7cac9594f8
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2023-07-28 21:04:01 +0530

    Largest Odd Number in a String

---
+++ LargestOddNumber.java
@@ -0,0 +1,11 @@
+//Longest Odd Number in String
+class Solution {
+    public String largestOddNumber(String num) {
+        int n = num.length();
+        for(int i=n-1; i>=0; i--){
+            if(num.charAt(i)%2 == 1)
+                return num.substring(0,i+1);
+        }
+        return "";
+    }
+}
