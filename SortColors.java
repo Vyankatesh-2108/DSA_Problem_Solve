381dffc44ecd5d5637dcfbbd3bc6cf8dd1509356
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2023-07-31 11:23:37 +0530

    Sort Colors by 0's, 1's and 2's

---
+++ SortColors.java
@@ -0,0 +1,31 @@
+//Sort Colors by 0's, 1's and 2's
+class Solution {
+    public void sortColors(int[] nums) {
+        int zeros=0, ones=0, twos=0;
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            if(nums[i] == 0)
+                zeros++;
+            else if(nums[i] == 1)
+                ones++;
+            else
+                twos++;
+        }
+        int j=0;
+        while(zeros > 0){
+            nums[j] = 0;
+            zeros--;
+            j++;
+        }
+        while(ones > 0){
+            nums[j] = 1;
+            ones--;
+            j++;
+        }
+        while(twos > 0){
+            nums[j] = 2;
+            twos--;
+            j++;
+        }
+    }
+}
