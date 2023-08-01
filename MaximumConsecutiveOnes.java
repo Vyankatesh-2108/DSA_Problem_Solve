77b4f746bfbe47e9897dcd582588ed3352951eae
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2023-08-01 09:25:33 +0530

    Maximum Cosecutive Ones

---
+++ MaximumConsecutiveOnes.java
@@ -0,0 +1,21 @@
+//Maximum Consecutive Ones
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count =0;
+        int max=0;
+        int m=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 1){
+                count++;
+                max=count;
+            }else{
+                count=0;
+            }
+            if(max > m){
+                m=max;
+            }   
+        }
+        return m;
+    }
+}
