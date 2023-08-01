4b5138dad57e4ea22d923c603c8221544f86cfc7
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2023-08-01 15:49:58 +0530

    Remove Element from Array

---
+++ RemoveElement.java
@@ -0,0 +1,15 @@
+//Remove Element
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n=nums.length;
+        int j=0;
+        for(int i=0; i<n; i++){
+            if(nums[i] != val){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
