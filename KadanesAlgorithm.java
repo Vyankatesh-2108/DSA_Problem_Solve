47bf33a6f4e2493c24e697e882c51608b89f1c54
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2023-08-07 17:11:01 +0530

    Kadanes Algorithm

---
+++ KadanesAlgorithm.java
@@ -0,0 +1,18 @@
+//Kadanes Algorithm
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int sum=0;
+        int maxi = nums[0];
+        for(int i=0; i<n; i++){
+            sum = sum+nums[i];
+	    //maxi = Math.max(maxi, sum);
+            if(sum>maxi)
+                maxi=sum;
+            if(sum<0)
+                sum = 0;
+        }
+        return maxi;
+    }
+}
