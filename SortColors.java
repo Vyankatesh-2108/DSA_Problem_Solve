051494f81aee1ce587666225800abb5400a2d258
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2023-08-08 20:32:31 +0530

    Sort Colors in two ways

--- SortColors.java
+++ SortColors.java
@@ -1,6 +1,8 @@
 //Sort Colors by 0's, 1's and 2's
 class Solution {
     public void sortColors(int[] nums) {
+	//We can Also use 
+	//Arrays.sort(nums);
         int zeros=0, ones=0, twos=0;
         int n = nums.length;
         for(int i=0; i<n; i++){
