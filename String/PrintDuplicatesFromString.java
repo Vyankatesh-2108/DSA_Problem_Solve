4bbd6bb02662ae119677262c4b69a45f49b8f928
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2023-08-15 22:30:28 +0530

    Remove Duplicates from string

---
+++ String/PrintDuplicatesFromString.java
@@ -0,0 +1,22 @@
+//print Duplicates from string
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	String str = "GeeksforGeeks";
+	char[] ch = str.toCharArray();
+	Map<Character, Integer> mp = new HashMap<>();
+	for(char chara : ch){
+	    if(mp.containsKey(chara))
+	        mp.put(chara, mp.get(chara)+1);
+	    else   
+	        mp.put(chara, 1);
+	}
+	
+	for(Map.Entry<Character, Integer> entry : mp.entrySet()){
+	    if(entry.getValue() > 1)
+	        System.out.println(entry.getKey() + " : "+entry.getValue());
+	}
+	
+	}
+}
