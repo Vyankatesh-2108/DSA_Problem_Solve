e8c01cef5bc384ceacbab29e93cdceb1d7cdd18d
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2023-08-20 23:28:33 +0530

    Search Roated Sorted Element in Array II

---
+++ BinarySearch/SearchRotatedSortedArrayII.java
@@ -0,0 +1,11 @@
+//Search in Rotated Sorted Array II
+
+class Solution {
+    public boolean search(int[] nums, int target) {
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == target)
+                return true;
+        }
+        return false;
+    }
+}
