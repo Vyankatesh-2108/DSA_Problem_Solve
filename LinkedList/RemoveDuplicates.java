4c69faa2ed3db4c998e989b505f9b7a4ec7f1b20
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2023-08-31 09:32:58 +0530

    Remove Duplicates from Sorted LL

---
+++ LinkedList/RemoveDuplicates.java
@@ -0,0 +1,14 @@
+//Remove Duplicates from sorted LL
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while(current != null && current.next != null){
+            if(current.val == current.next.val)
+                current.next = current.next.next;
+            else
+                current = current.next;
+        }
+        return head;
+    }
+}
