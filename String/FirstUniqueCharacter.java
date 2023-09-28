5bdc66dc52fd9afda48a7889e7c9d609e4777051
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2023-09-28 20:48:35 +0530

    First Unique Character

---
+++ String/FirstUniqueCharacter.java
@@ -0,0 +1,14 @@
+//First Unique Character in a String
+
+class Solution {
+    public int firstUniqChar(String s) {
+       int[] temp = new int[26];
+        for(int i=0; i<s.length(); i++)
+            temp[s.charAt(i)-'a']++;
+        for(int i=0; i<s.length(); i++){
+            if(temp[s.charAt(i)-'a'] == 1)
+                return i;
+        }
+        return -1;
+    }
+}
