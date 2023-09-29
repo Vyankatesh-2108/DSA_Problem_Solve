401c849f32d608faee4cc6355c0a582a68fbca4d
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2023-09-29 18:09:01 +0530

    Maximum Product

---
+++ Arrays/MaximumProduct.java
@@ -0,0 +1,11 @@
+//Maximum product of three number
+
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        int case1 = nums[0]*nums[1]*nums[n-1];
+        int case2 = nums[n-1]*nums[n-2]*nums[n-3];
+        return Integer.max(case1, case2);
+    }
+}
