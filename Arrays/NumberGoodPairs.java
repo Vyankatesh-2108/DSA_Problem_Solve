d6b9a0b35ac23f6d98526700f11338243df95e9c
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2023-10-07 22:51:08 +0530

    Number of Good Pair

---
+++ Arrays/NumberGoodPairs.java
@@ -0,0 +1,14 @@
+//Number of Good Pairs
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int count=0;
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(i<j && nums[i]==nums[j])
+                    count++;
+            }
+        }
+        return count;
+    }
+}
