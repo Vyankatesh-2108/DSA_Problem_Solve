98d0b4675000008d5a9a3311790d3d42f4ac57d3
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2023-11-27 22:08:43 +0530

    Number of Segment in a string

---
+++ Common/numberOfSeg.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int countSegments(String s) {
+        StringTokenizer s1 = new StringTokenizer(s, "\s");
+        int c=0;
+        while(s1.hasMoreTokens()){
+            c++;
+            s1.nextToken();
+        }
+        return c;
+    }
+}
