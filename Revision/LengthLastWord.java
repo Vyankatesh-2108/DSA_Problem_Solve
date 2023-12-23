b0624016df3affa793a55716f7b13467328e74c1
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2023-12-23 17:01:36 +0530

    Length of Last Word

---
+++ Revision/LengthLastWord.java
@@ -0,0 +1,16 @@
+//Length of Last Word
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n = s.length()-1;
+        while(s.charAt(n) == ' ')
+            n--;
+        int c=0;
+        for(int i=n; i>=0; i--){
+            if(s.charAt(i) == ' ')
+                break;
+            c++;
+        }
+        return c;
+    }
+}
