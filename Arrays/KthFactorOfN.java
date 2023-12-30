19de5adab2971858fcaa9427fba8a001d1786940
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2023-12-30 10:17:44 +0530

    Kth Factor of N

---
+++ Arrays/KthFactorOfN.java
@@ -0,0 +1,13 @@
+//Kth Factor of n
+
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+       for(int i=1; i<=n/2; i++){
+           if(n%i == 0)
+               if(--k == 0)
+                return i;
+       }
+       return k==1 ? n : -1;
+    }
+};
