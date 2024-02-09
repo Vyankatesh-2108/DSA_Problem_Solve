931915730c66bf119bf8b82cf3e2d0750f854a81
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2024-02-09 10:24:10 +0530

    The Kth Factor

---
+++ Maths/theKthfactor.java
@@ -0,0 +1,13 @@
+//the Kth factor
+
+class Solution {
+    public int kthFactor(int n, int k) {
+        for(int i=1; i<=n; i++){
+            if(n%i == 0){
+                if(--k == 0)
+                    return i;
+            }
+        }
+        return -1;
+    }
+}
