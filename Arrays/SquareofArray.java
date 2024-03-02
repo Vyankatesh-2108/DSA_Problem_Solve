6f8e58659d143c97f3f8ecda0b8df3e45cd0d303
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2024-03-02 16:36:29 +0530

    Square of Sorted Array

---
+++ Arrays/SquareofArray.java
@@ -0,0 +1,11 @@
+//Square of Array
+
+class Solution {
+    public int[] sortedSquares(int[] nums) { 
+        for(int i=0; i<nums.length; i++){
+            nums[i] = Math.abs(nums[i]*nums[i]);
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
