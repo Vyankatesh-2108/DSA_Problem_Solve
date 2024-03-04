d1c2662008e3b1cb02266bf143df81946421a62f
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2024-03-04 21:28:22 +0530

    Check If N and Its Double Exist

---
+++ Arrays/ChechifnanditsDoubleexist.java
@@ -0,0 +1,13 @@
+//Chech if n and its Double exist
+
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length; j++){
+                if(arr[i]==2*arr[j] && i != j)
+                    return true;
+            }
+        }
+        return false;
+    }
+}
