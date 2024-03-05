006e89263474281241d1db3970b5f9abe40531b1
Vyankatesh-2108 <vyankateshkhetri07@gmail.com>
2024-03-05 15:50:12 +0530

    Top k frequent elements

---
+++ Arrays/TopkFrequentelements.java
@@ -0,0 +1,24 @@
+//Top k Frequent elements
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i:nums)
+        {
+            if(map.containsKey(i))
+            {
+                map.put(i,map.get(i)+1);
+            }
+            else{
+                map.put(i,1);
+            }
+        }
+        List<Integer>list=new ArrayList<>(map.keySet());
+        list.sort((a,b)->map.get(b)-map.get(a));
+        int res[] = new int[k];
+        for (int i = 0; i < k; ++i)
+            res[i] = list.get(i);
+        return res;
+
+    }
+}
